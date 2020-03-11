33c2d7cc2c2750f7ecc66bfff5be36912865466b
Victor Calancea <victor.calancea@abcsupply.com>
2020-03-10 23:13:57 -0500

    Section 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import QuizQuestion from './QuizQuestion'
+import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
 
-let quizData = require('./quiz_data.json')
+let quizData = require("./quiz_data.json");
 
 class Quiz extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { quiz_position: 1 }
-        this.question = quizData.quiz_questions[0]
-    }
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+    this.question = quizData.quiz_questions[0];
+  }
 
-    render() {
-        return <div>
-            <QuizQuestion 
-                quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-        </div>
+  render() {
+    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    
+    if (isQuizEnd) {
+        return <QuizEnd />
+    } else {
+        return <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
     }
+  }
 }
 
-export default Quiz
+export default Quiz;
